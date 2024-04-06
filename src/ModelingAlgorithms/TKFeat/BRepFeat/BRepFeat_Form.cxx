e8e2758d6f667676c7170ec84f7f20b08232cfb9
Zheng, Lei <realthunder.dev@gmail.com>
2024-04-06 17:28:36 +0800

    Fix BRepFeat_MakePrism empty result on JustFeat
    Fixes realthunder/FreeCAD#985

--- src/ModelingAlgorithms/TKFeat/BRepFeat/BRepFeat_Form.cxx
+++ src/ModelingAlgorithms/TKFeat/BRepFeat/BRepFeat_Form.cxx
@@ -799,7 +799,14 @@ void BRepFeat_Form::GlobalPerform()
     } // if(expp.More() && !Comp.IsNull() && !myGShape.IsNull())  {
     //
 
-    if (myJustFeat && myPerfSelection == BRepFeat_NoSelection)
+    // If we consider myPrefSelection, e.g. BRepFeat_SelectionU, the feature
+    // might be discarded if UntilInShape is true and the generated prism shape
+    // is inside the base. However, this would be perfectly normal if the
+    // caller wants to do a cut from base on its own. So, just return the
+    // feature without filtering.
+    //
+    // if(myJustFeat && myPerfSelection == BRepFeat_NoSelection)
+    if (myJustFeat)
     {
       myShape = theGShape;
       Done();
